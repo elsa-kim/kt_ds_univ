4a4a66bf5935b5171802e5b81d2b0c49256e147b
Sohyun Kim <ksh96611@naver.com>
2024-01-30 17:18:04 +0900

    Create IfNumberFormatException.java

---
+++ Java/code/exception_handling/IfNumberFormatException.java
@@ -0,0 +1,20 @@
+
+public class IfNumberFormatException {
+
+	public static void main(String[] args) {
+		
+		String numString = "123";
+		if (numString.matches("^[0-9]+$")) {
+			int num = Integer.parseInt(numString);
+			System.out.println(num);
+		}
+		
+		numString = "ABC";
+		if (numString.matches("^[0-9]+$")) {
+			int num = Integer.parseInt(numString);
+			System.out.println(num);
+		}
+	
+	}
+
+}
