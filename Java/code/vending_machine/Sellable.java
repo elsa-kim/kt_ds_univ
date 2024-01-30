3dcdcc9dc4fb990ef93beda20e24b882ead1e24b
Sohyun Kim <ksh96611@naver.com>
2024-01-30 17:27:14 +0900

    Create Sellable.java

---
+++ Java/code/vending_machine/Sellable.java
@@ -0,0 +1,47 @@
+package vending_machine;
+
+public interface Sellable {
+
+	// interface에서 상수 정의할 때.
+	// static final은 자동으로 적용되므로 생략한다.
+//	public static final int PRODUCT_COUNT = 1;
+//	public static final String MACHINE_NAME = "자판기";
+	public int PRODUCT_COUNT = 1;
+	public String MACHINE_NAME = "자판기";
+	
+	// getter
+	public Product[] getProductArray();
+	
+	public int getMoney(); 
+	
+	//setter
+	public void setMoney(int money);
+	
+	/**
+	 * 돈을 넣는다
+	 * @param customer 돈을 넣은 고객
+	 * @param productName 구매할 제품명(제로펩시, 제로콜라, 제로스프라이트)
+	 */
+	public void insertMoney(Customer customer,String productName);
+	
+	/**
+	 * 버튼을 누른다
+	 * @param customer 버튼을 누른 고객
+	 * @param productName 구매할 제품명(제로펩시, 제로콜라, 제로스프라이트)
+	 */
+	public void pressButton(Customer customer, String productName);
+	
+	public void pressButton(Customer customer, String productName, int orderCount);
+	
+	// refund잘못된 사용을 막기위해 private으로 해야되는데, 인터페이스에선 public밖에 안됨 => 따로 빼줌
+//	/**
+//	 * 고객에게 환불처리한다
+//	 * 상속된 클래스에서만 사용할 수 있도록 한다
+//	 * @param customer 환불받을 고객
+//	 * @param refuneMoney 환불받을 금액
+//	 */
+//	 public void refund(Customer customer, int refundMoney);
+	 
+	 public void printProducts();
+	
+}
