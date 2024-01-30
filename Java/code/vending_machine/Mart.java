00ad61569ad839c2f8f6b4906b59701e630cc44b
Sohyun Kim <ksh96611@naver.com>
2024-01-30 17:25:01 +0900

    Update Mart.java

--- Java/code/vending_machine/Mart.java
+++ Java/code/vending_machine/Mart.java
@@ -8,6 +8,86 @@ public class Mart {
 	
 	
 	public static void main(String[] args) {
+		// 모든 클래스의 슈퍼클래스는 object
+		
+		/*
+		 * Object
+		 *   --> Product
+		 *  	--> TemperatureProduct
+		 *  Project is a Object
+		 *  TemperatureProduct is a Product
+		 *  TemperatureProduct is a Object
+		 */
+		
+		
+//		Product p = new Product();
+//		TemperatureProduct tp = new TemperatureProduct();
+//		
+////		printTemperatureProduct(tp);
+//		printProduct(tp);
+//		printProduct(p);
+		
+		
+		Product p = new Product();
+		p.setName("보드마카");
+		p.setPrice(500);
+		p.setQuantity(40);
+		
+		System.out.println(p);
+		//vending_machine.Product@279f2327
+		// 제품명 : 보드마카, 가격 : 500, 재고 : 40
+		
+		
+		// Seller(추상클래스)
+		// --> VendingMachine
+		// --> RefundableVendingMachine
+		// Is A (다형성)
+		// VendingMachine is a Seller
+		// RefundableVendingMachine is a Seller
+		// Seller drinkVendingMachine = new VendingMachine();
+		// Seller drinkVendingMachine = new RefundableVendingMachine();
+		
+		//Sellable(인터페이스)
+		// --> VendingMachine
+		// --> RefundableVendingMachine
+		// 인터페이스를 구현(Is A)
+		// VendingMachine is a Sellable
+		// RefundableVendingMachine is a Sellable
+		// Sellable drinkVendingMachine = new VendingMachine();
+		// Sellable drinkVendingMachine = new RefundableVendingMachine();
+		
+		//객체지향 방식으로 개발
+		Sellable drinkMachine = new VendingMachine();
+		
+		
+		Customer musk = new Customer(200_000);
+		
+		
+		// 상품 하나씩 구매
+		drinkMachine.insertMoney(musk, "제로펩시");
+		drinkMachine.pressButton(musk, "제로펩시", 50);
+		
+		drinkMachine.insertMoney(musk, "제로펩시");
+		drinkMachine.pressButton(musk, "제로펩시");
+		
+		drinkMachine.printProducts();
+		musk.printProducts();
+		
+		
+		Sellable snackMachine = new RefundableVendingMachine(400);
+		// 상품 n개씩 구매
+		snackMachine.insertMoney(musk, "제로펩시");
+		snackMachine.pressButton(musk, "제로펩시", 50);
+		
+		snackMachine.insertMoney(musk, "제로펩시");
+		snackMachine.pressButton(musk, "제로펩시", 2);
+		
+		System.out.println("-------");
+		snackMachine.printProducts();
+		musk.printProducts();
+		
+		
+	/*
 		
 		//객체지향 방식으로 개발
 		VendingMachine drinkMachine = new VendingMachine();
@@ -26,13 +106,8 @@ public class Mart {
 		System.out.println("고객의 상품 수량 : "+ musk.product.quantity);
 		
 		
-		DrinkMachine A = new DrinkMachine();
-		Product orderProduct = A.order("박카스", 18);
-		System.out.println(">> 주문결과: "+ orderProduct.name + ", " + "개당 "+orderProduct.price + "원, 주문수량 " + orderProduct.quantity);
-		Product orderProduct2 = A.order("몬스터", 2);
-		System.out.println(">> 주문결과: "+ orderProduct2.name + ", " + "개당 "+orderProduct2.price + "원, 주문수량 " + orderProduct2.quantity);
-		A.order("박카", 5);
-		A.printStock();
+		
+		*/
 	}
 
 }
