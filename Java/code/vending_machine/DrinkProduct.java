944fa01361558462a3eea3cb60a8e3939ac5e73b
Sohyun Kim <ksh96611@naver.com>
2024-01-30 17:25:40 +0900

    Create DrinkProduct.java

---
+++ Java/code/vending_machine/DrinkProduct.java
@@ -0,0 +1,22 @@
+package vending_machine;
+
+public class DrinkProduct {
+	
+
+	/**
+	 * 상품의 이름
+	 */
+	String name;
+	
+	/**
+	 * 상품의 가격
+	 */
+	int price;
+	
+	/**
+	 * 상품의 재고
+	 */
+	int quantity;
+	
+
+}
