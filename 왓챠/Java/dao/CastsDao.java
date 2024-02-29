f76bfef4cbcb8c37b8ee931ea187b3f9dd3a0000
Sohyun Kim <ksh96611@naver.com>
2024-02-29 16:09:56 +0900

    Create CastsDao.java

---
+++ 왓챠/Java/dao/CastsDao.java
@@ -0,0 +1,30 @@
+package com.ktdsuniversity.watcha.dao;
+
+import com.ktdsuniversity.watcha.util.DBSupporter;
+import com.ktdsuniversity.watcha.vo.ActorsVO;
+import com.ktdsuniversity.watcha.vo.CastsVO;
+
+public class CastsDao {
+	
+	
+	public int insertNewActor(DBSupporter dbSupporter, CastsVO castsVO) {
+		
+		StringBuffer query = new StringBuffer();
+		query.append(" INSERT INTO CASTS ");
+		query.append("  (CAST_ID ");
+		query.append(" , ACTOR_ID ");
+		query.append(" , MOVIE_ID ");
+		query.append(" , CHARACTER_NAME ");
+		query.append(" , MAIN_ACTOR_YN) ");
+		query.append(" VALUES ");
+		query.append("  ('CT-' || TO_CHAR(SYSDATE, 'YYYYMMDD') || '-' || LPAD(SEQ_CASTS_PK.NEXTVAL, 6, '0')/*CAST_ID*/ ");
+		query.append(" , ?/*ACTOR_ID*/ ");
+		query.append(" , ?/*MOVIE_ID*/ ");
+		query.append(" , ?/*CHARACTER_NAME*/ ");
+		query.append(" , ?/*MAIN_ACTOR_YN*/) ");
+		
+		
+		return dbSupporter.insert(query.toString(), new Object[] {castsVO.getActorId(), castsVO.getMovieId(), castsVO.getCharacterName(), castsVO.getMainActorYn()});
+	}
+
+}
