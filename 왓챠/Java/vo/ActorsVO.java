4806f4cfd7a8c7181fc43be481ebc3062eea24fd
Sohyun Kim <ksh96611@naver.com>
2024-02-29 16:18:59 +0900

    Create ActorsVO.java

---
+++ 왓챠/Java/vo/ActorsVO.java
@@ -0,0 +1,39 @@
+package com.ktdsuniversity.watcha.vo;
+
+import java.util.List;
+
+public class ActorsVO {
+	
+	private String actorId;
+	private String profile;
+	private String name;
+	
+	private List<CastsVO> casts;
+	
+	public String getActorId() {
+		return actorId;
+	}
+	public void setActorId(String actorId) {
+		this.actorId = actorId;
+	}
+	public List<CastsVO> getCasts() {
+		return casts;
+	}
+	public void setCasts(List<CastsVO> casts) {
+		this.casts = casts;
+	}
+	public String getProfile() {
+		return profile;
+	}
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+}
