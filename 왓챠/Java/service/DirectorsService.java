17e9e00a42d1eb2c355579853aab6066f412f013
Sohyun Kim <ksh96611@naver.com>
2024-03-04 17:17:23 +0900

    핸들러 사용 메소드 추가

--- 왓챠/Java/service/DirectorsService.java
+++ 왓챠/Java/service/DirectorsService.java
@@ -2,8 +2,10 @@ package com.ktdsuniversity.watcha.service;
 
 import java.util.List;
 
+import com.ktdsuniversity.watcha.dao.CastsDao;
 import com.ktdsuniversity.watcha.dao.DirectorsDao;
 import com.ktdsuniversity.watcha.dao.MoviesDao;
+import com.ktdsuniversity.watcha.dao.ProducingDao;
 import com.ktdsuniversity.watcha.util.DBSupporter;
 import com.ktdsuniversity.watcha.vo.DirectorsVO;
 import com.ktdsuniversity.watcha.vo.MoviesVO;
@@ -16,10 +18,12 @@ public class DirectorsService {
 	
 	private DirectorsDao directorsDao;
 	private MoviesDao moviesDao;
+	private ProducingDao producingDao;
 	
 	public DirectorsService() {
 		this.directorsDao = new DirectorsDao();
 		this.moviesDao = new MoviesDao();
+		this.producingDao = new ProducingDao();
 	}
 	
 	public boolean createNewDirector(String directorsName, String directorsProfile) {
@@ -51,7 +55,7 @@ public class DirectorsService {
 		
 		// 데이터베이스에 등록된 모든 감독을 조회한다.
 		// select는 트랜잭션 처리 필요 없지만 DB연결 위해 서포터 사용(dbSupporter.open())
-		List<DirectorsVO> directors = this.directorsDao.selectAllDirector(dbSupporter);
+		List<DirectorsVO> directors = this.directorsDao.selectAllDirectors(dbSupporter);
 
 		// 감독목록을 반복하면서 해당 감독이 제작한 영화 목록을 조회한다.
 		directors.forEach((director) -> {
@@ -62,6 +66,64 @@ public class DirectorsService {
 		
 		dbSupporter.close(); // Database와 연결을 해제한다.
 		return directors;
+	}
+
+	public List<DirectorsVO> findAllDirectors() {
+		DBSupporter dbSupporter = new DBSupporter("WATCHA","WATCHA");
+		dbSupporter.open();
+
+		List<DirectorsVO> directors = this.directorsDao.selectAllDirectors(dbSupporter);
+
+		
+		dbSupporter.close(); 
+		return directors;
+	}
+
+	public List<DirectorsVO> findDirectorsByName(String name) {
+		DBSupporter dbSupporter = new DBSupporter("WATCHA","WATCHA");
+		dbSupporter.open();
+
+		List<DirectorsVO> directors = this.directorsDao.selectDirectorsByName(dbSupporter, name);
+
+		
+		dbSupporter.close(); 
+		return directors;
+	}
+
+	public DirectorsVO findDirectorById(String directorsId) {
+		DBSupporter dbSupporter = new DBSupporter("WATCHA","WATCHA");
+		dbSupporter.open();
+
+		DirectorsVO directorsVO = this.directorsDao.selectDirectorById(dbSupporter, directorsId);
+
+		
+		dbSupporter.close(); 
+		return directorsVO;
+	}
+
+	public boolean modifyOneDirector(DirectorsVO directorsVO) {
+		DBSupporter dbSupporter = new DBSupporter("WATCHA","WATCHA");
+		dbSupporter.open();
+
+		int updatedCount = this.directorsDao.updateOneDirector(dbSupporter, directorsVO);
+
+		
+		dbSupporter.close(); 
+		return updatedCount > 0;
+	}
+
+	public boolean deleteOneDirectorById(String directorsId) {
+		DBSupporter dbSupporter = new DBSupporter("WATCHA","WATCHA");
+		dbSupporter.open();
+		
+		int deletedProducingCount = this.producingDao.deleteProducingByDirectorsId(dbSupporter, directorsId);
+		System.out.println(deletedProducingCount + "건의 제작 정보가 삭제되었습니다.");
+
+		int deletedCount = this.directorsDao.deleteOneDirectorById(dbSupporter, directorsId);
+
+		
+		dbSupporter.close(); 
+		return deletedCount > 0;
 	}
 	
 }
